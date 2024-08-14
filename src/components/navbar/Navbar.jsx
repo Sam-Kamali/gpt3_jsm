af61cfd81d4364c019ab2477db89b0e8b3d4ca81
sam kamali <kama0104@algonquinlive.com>
2024-08-13 22:16:34 -0400

    imports assets

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import { RiMenu3Line, RiCloseLin } from 'react-icons/ri';
 import './navbar.css';
+//naming the classNames {css naming convention} BEM --> Block Element Modifier
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='gpt3__navbar'>
+      <div className="gp3__navbar-links">
+        <div className="gpt3__navbar-links_logo">
+          <img src={logo} alt="logo" />
+        </div>
+
+      </div>
+    </div>
   )
 }
 
